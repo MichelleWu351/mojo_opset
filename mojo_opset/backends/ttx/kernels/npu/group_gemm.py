4ab346978bd220364770b7ad6153e710a55b698c
kevin-hongkai <253715840@qq.com>
2026-05-20 18:56:23 +0800

    add sync_solver=False to avoid groupgemm perf descend on triton-ascend

--- mojo_opset/backends/ttx/kernels/npu/group_gemm.py
+++ mojo_opset/backends/ttx/kernels/npu/group_gemm.py
@@ -224,6 +224,7 @@ def m_grouped_matmul_impl(
         strideBN,
         strideBK,
         multibuffer=True,
+        sync_solver=False,
     )
     return C
 
@@ -323,5 +324,5 @@ def k_grouped_matmul_impl(
         assert M % META["BLOCK_M"] == 0, "Only support when M is a multiple of BLOCK_M"
         return (num_cores,)
 
-    _k_grouped_matmul_kernel[grid](A, B, C, size_per_group, num_groups, M, N, multibuffer=True)
+    _k_grouped_matmul_kernel[grid](A, B, C, size_per_group, num_groups, M, N, multibuffer=True,sync_solver=False)
     return C
